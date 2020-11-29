e16c22547124aa8c90fe0e8430e62c582a0143ce
DJStefan <stefan.kussmaul@gmail.com>
2020-11-29 10:15:35 +0100

    Improved CLI interface, added run_script command

--- data-importer-python/manage_db.py
+++ data-importer-python/manage_db.py
@@ -1,11 +1,48 @@
 # Command-line interface for managing our MySQL Landtagswahl database.
 # Will provide functions to import election results from XML.
 import click
+import mysql.connector
 import test_db
 
 
+@click.group()
+def click_cli():
+    """Create the command group for the Click command-line interface."""
+    pass
+
+
+# TODO: REQUIRE PASSWORD INPUT AS PROMPT
+@click_cli.command(name='run_script')
+@click.argument('filepath', type=click.Path(exists=True, readable=True))
+@click.option('--host', type=str, default='localhost')
+@click.option('--user', type=str, default='root')
+@click.option('--password', type=str, required=True)  
+def cmd_reset_database(
+    filepath: str,
+    host: str,
+    user: str,
+    password: str,
+):
+    click.echo('Reading "{}"'.format(filepath))
+    with open(filepath) as sql_file:
+        schema_script = sql_file.read()
+
+    click.echo('Connecting to database...')
+    mydb = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+    )
+
+    click.echo('Executing script..')
+    mycursor = mydb.cursor()
+    mycursor.execute(schema_script, multi=True)
+    mydb.commit()
+    click.echo('Done')
+
+
 # Note: We could use the `click.password_option()` to hide the password in console
-@click.command()
+@click_cli.command(name='import_data')
 @click.argument('datafile', type=click.Path(exists=True, readable=True))
 @click.option('--host', type=str, default='localhost')
 @click.option('--user', type=str, default='root')
@@ -23,4 +60,4 @@ def cmd_load_datafile(
 
 
 if __name__ == '__main__':
-    cmd_load_datafile()
+    click_cli()
