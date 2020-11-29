d1c6fb7a4c7928a3337474d96172f9e078c61164
DJStefan <stefan.kussmaul@gmail.com>
2020-11-29 12:22:35 +0100

    Added lookups to record in-database IDs

--- data-importer-python/data_importer.py
+++ data-importer-python/data_importer.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import database as db
+import util
 import data_parser
 
 
@@ -10,25 +11,28 @@ def run_import(
     results_xml: data_parser.ParsedResultsXML,
     year: int,
 ):
-    cur = database.get_cursor()
-
     # Add Wahl to database and get wahlID
     if database.has_wahl(year):
         wahl_id = database.get_wahl_id(year)
     else:
         database.add_wahl(year)
         wahl_id = database.get_wahl_id(year)
-    print(wahl_id)
     
-    # Add Stimmkreis data and TODO: record IDs
+    # Map stimmkreis number to in-database ID
+    stimmkreis_id_lookup: dict[int, int] = {}
+
+    # Add Stimmkreis data and record IDs
     for stimmkreis in info_xml.stimmkreise.values():
         stimmkreis_id = database.add_stimmkreis(
             wahl_id,
             stimmkreis,
         )
+        stimmkreis_id_lookup[stimmkreis.region_id] = stimmkreis_id
 
-    # Add Party data and TODO: record IDs
+    # Map party names to in-database ID
     party_id_lookup: dict[str, int] = {}
+
+    # Add Party data and record IDs
     for party_name in results_xml.parties:
         party_id = database.add_party(
             wahl_id,
@@ -36,10 +40,14 @@ def run_import(
         )
         party_id_lookup[party_name] = party_id
 
-    # Add Candidate data and TODO: record IDs
+    # Map Candidate instances to in-database ID
+    candidate_id_lookup: dict[util.Candidate, int] = {}
+
+    # Add Candidate data and record IDs
     for candidate in results_xml.candidates:
         candidate_id = database.add_candidate(
             wahl_id,
             candidate,
             party_id_lookup[candidate.party],
         )
+        candidate_id_lookup[candidate] = candidate_id
