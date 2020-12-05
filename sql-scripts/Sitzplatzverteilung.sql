01a2d9a633c0572e9b0afb33177f0010205ea4c8
Vladyslav Kolesnykov <vlad.kolesnykov@gmail.com>
2020-12-05 15:46:27 +0100

    View for the first votes takes into account also an election year

--- sql-scripts/Sitzplatzverteilung.sql
+++ sql-scripts/Sitzplatzverteilung.sql
@@ -1,15 +1,15 @@
 -- Anzahl Erststimme für jeden Kandidat (sortiert nach Stimmkreis und Anzahl Stimme)
 CREATE OR REPLACE VIEW Kandidat_Stimmkreis_Erststimme AS
-SELECT Kandidat, Stimmkreis, count(StimmeID) as Anzahl FROM bayerische_landtagswahl.Erststimme
+SELECT Wahl, Kandidat, Stimmkreis, count(StimmeID) as Anzahl FROM bayerische_landtagswahl.Erststimme
 WHERE IsValid = 1
-GROUP BY Kandidat, Stimmkreis
-ORDER BY Stimmkreis, Anzahl DESC;
+GROUP BY Wahl, Kandidat, Stimmkreis
+ORDER BY Wahl, Stimmkreis, Anzahl DESC;
 
 -- TODO: implement 5% check for each candidat
 -- Erststimme Gewinner für jeden Stimmkreis mit der Anzahl Stimme
 CREATE OR REPLACE VIEW Erststimme_Gewinner_Stimmkreis AS
-SELECT kse.Kandidat, kse.Stimmkreis, kse.Anzahl FROM Kandidat_Stimmkreis_Erststimme kse
-INNER JOIN (SELECT Stimmkreis, MAX(Anzahl) as Anzahl FROM Kandidat_Stimmkreis_Erststimme GROUP BY Stimmkreis) kse_grouped
-ON kse.Anzahl = kse_grouped.Anzahl AND kse.Stimmkreis = kse_grouped.Stimmkreis;
+SELECT kse.Wahl, kse.Kandidat, kse.Stimmkreis, kse.Anzahl FROM Kandidat_Stimmkreis_Erststimme kse
+INNER JOIN (SELECT Wahl, Stimmkreis, MAX(Anzahl) as Anzahl FROM Kandidat_Stimmkreis_Erststimme GROUP BY Wahl, Stimmkreis) kse_grouped
+ON kse.Wahl = kse_grouped.Wahl AND kse.Anzahl = kse_grouped.Anzahl AND kse.Stimmkreis = kse_grouped.Stimmkreis;
 
 SELECT * FROM Erststimme_Gewinner_Stimmkreis
