13643681330400d274fc7b9559c2d0e0d5aaa6ab
DJStefan <stefan.kussmaul@gmail.com>
2020-12-19 11:55:32 +0100

    Changed dir structure, added database config file (#1)

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,9 @@
+The backend runs using a Python Flask webserver.
+
+# Initial Setup
+
+Edit `db-config.json` and insert the values you need to connect to your database. Do not commit `db-config.json`!
+
 # Running the server
 
 Instructions from [the Flask Tutorial](https://flask.palletsprojects.com/en/1.1.x/tutorial/factory/#run-the-application):
--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,14 +1,31 @@
 import os
-
+import pathlib
+import json
 from flask import Flask
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+
+    instance_path = pathlib.Path(app.instance_path)
+
+    # Create the instance folder if it doesn't already exist
+    instance_path.mkdir(exist_ok=True)
+
+    # Load the secret config
+    dbconfig_path = instance_path / 'db-config.json'
+    with open(dbconfig_path, 'r') as secret_file:
+        db_config = json.load(secret_file)
+        print(db_config)
+        # TODO: MAKE SURE ALL VALUES HAVE BEEN SET
+
+    # Add database config to the app config
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DB_HOST=db_config['host'],
+        DB_USER=db_config['user'],
+        DB_PASSWORD=db_config['password'],
+        DB_NAME=db_config['database_name'],
     )
 
     if test_config is None:
