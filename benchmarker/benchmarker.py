b2e411dea9e47cc6dfcc93009de968aeaa7c3435
DJStefan <stefan.kussmaul@gmail.com>
2021-01-10 14:39:40 +0100

    Moved benchmark logic into Terminal class (#38)

--- benchmarker/benchmarker.py
+++ benchmarker/benchmarker.py
@@ -1,54 +1,26 @@
 # TODO: MAKE INTO ITS OWN PACKAGE
-import urllib.request
-import time
-import dataclasses as dc
-import random
+import terminal
 
 
-@dc.dataclass
-class WorkRequest:
-    url: str
-    fraction: float
-
-
-@dc.dataclass
-class Workload:
-    work: list[WorkRequest]
-    avg_wait_sec: float
-
-
+base_url = 'http://localhost:3000/#'
 all_work = [
-    WorkRequest('/', 0.25),
-    WorkRequest('/mitglieder', 0.10),
-    WorkRequest('/stimmkreise', 0.25),
-    WorkRequest('/ueberhangmandate', 0.10),
-    WorkRequest('/sieger', 0.10),
-    WorkRequest('/sieger', 0.20),
+    terminal.WorkRequest('/', 0.25),
+    terminal.WorkRequest('/mitglieder', 0.10),
+    terminal.WorkRequest('/stimmkreise', 0.25),
+    terminal.WorkRequest('/ueberhangmandate', 0.10),
+    terminal.WorkRequest('/sieger', 0.10),
+    terminal.WorkRequest('/sieger', 0.20),
     # TODO: ADD KNAPPSTE-SIEGER PAGE
 ]
 avg_wait_sec = 0.2
 # num_terminals = 10
 num_requests_per_terminal = 10
 
-# Determine work plan (URLs to call)
-work_plan = random.choices(
-    [work.url for work in all_work],
-    weights=[work.fraction for work in all_work],
-    k=num_requests_per_terminal,
-)
-
-results = {url: [] for url in work_plan}
-for url in work_plan:
-    test_url = 'http://localhost:3000/#{}'.format(url)
-
-    start_time = time.perf_counter()
-    urllib.request.urlopen(test_url)
-    end_time = time.perf_counter()
-
-    load_time = end_time - start_time
-    results[url].append(load_time)
-
-    wait_time = random.uniform(0.8 * avg_wait_sec, 1.2 * avg_wait_sec)
-    time.sleep(wait_time)
-
-print(results)
+t = terminal.Terminal(terminal.Workload(
+    base_url,
+    all_work,
+    avg_wait_sec,
+    num_requests_per_terminal,
+))
+t.run()
+print(t.results)
---
+++ benchmarker/terminal.py
@@ -0,0 +1,49 @@
+import urllib.request
+import random
+import time
+import dataclasses as dc
+
+
+@dc.dataclass
+class WorkRequest:
+    url: str
+    fraction: float
+
+
+@dc.dataclass
+class Workload:
+    base_url: str
+    work: list[WorkRequest]
+    avg_wait_sec: float
+    num_requests: int
+
+
+class Terminal:
+    def __init__(
+            self,
+            workload: Workload,
+    ):
+        self.workload = workload
+
+        # Determine work plan (URLs to call)
+        self._work_plan = random.choices(
+            [work.url for work in workload.work],
+            weights=[work.fraction for work in workload.work],
+            k=workload.num_requests,
+        )
+
+        self.results = {_url: [] for _url in self._work_plan}
+
+    def run(self):
+        for _url in self._work_plan:
+            test_url = self.workload.base_url + _url
+
+            start_time = time.perf_counter()
+            urllib.request.urlopen(test_url)
+            end_time = time.perf_counter()
+
+            load_time = end_time - start_time
+            self.results[_url].append(load_time)
+
+            wait_time = random.uniform(0.8 * self.workload.avg_wait_sec, 1.2 * self.workload.avg_wait_sec)
+            time.sleep(wait_time)
