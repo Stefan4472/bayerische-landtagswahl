d3afb2474fd1cfc08f0ae65a034f60058ab2795d
Vladyslav Kolesnykov <vlad.kolesnykov@gmail.com>
2021-02-03 16:01:11 +0100

    changed MATERIALIZED VIEW drop and refresh

--- sql-scripts/UpdateViews.sql
+++ sql-scripts/UpdateViews.sql
@@ -1,20 +1,22 @@
 REFRESH MATERIALIZED VIEW Erststimme_Kandidat;
-REFRESH MATERIALIZED VIEW Anzhal_Zweitstimme_Kandidat;
 REFRESH MATERIALIZED VIEW Gesamtstimmen_Partei_Stimmkreis;
 REFRESH MATERIALIZED VIEW Gesamtstimmen_Partei_Wahl;
-REFRESH MATERIALIZED VIEW Gesamtstimmen_Partei_Wahl;
 REFRESH MATERIALIZED VIEW Erststimme_Gewinner_Pro_Stimmkreis;
-REFRESH MATERIALIZED VIEW Gesamtstimmen_und_Sitze_Partei_5Prozent_Wahlkreis;
+-- should be also updated
+-- DROP TABLE IF EXISTS Sitze_Wahlkreise CASCADE;
+REFRESH MATERIALIZED VIEW Sitze_Partei_Vor_Ausgleich;
+REFRESH MATERIALIZED VIEW Gesamtstimmen_und_Sitze_Partei;
 REFRESH MATERIALIZED VIEW Mitglieder_des_Landtages;
+REFRESH MATERIALIZED VIEW Knappste_Sieger_Verlierer;
 REFRESH MATERIALIZED VIEW Sitzverteilung;
 REFRESH MATERIALIZED VIEW Mitglieder_des_LandtagesUI;
 REFRESH MATERIALIZED VIEW WahlbeteiligungUI;
 REFRESH MATERIALIZED VIEW Gesamtstimmen_Partei_StimmkreisUI;
 REFRESH MATERIALIZED VIEW DirektkandidatenUI;
+REFRESH MATERIALIZED VIEW Entwicklung_Stimmen_2018_zum_2013UI;
 REFRESH MATERIALIZED VIEW StimmkreissiegerUI;
 REFRESH MATERIALIZED VIEW UeberhangmandateUI;
-REFRESH MATERIALIZED VIEW Knappste_Sieger_Verlierer;
 REFRESH MATERIALIZED VIEW KnappsteSiegerUI;
 REFRESH MATERIALIZED VIEW KnappsteVerliererUI;
-REFRESH MATERIALIZED VIEW Durchschnitt_Stimmen_Pro_VornameUI;
+REFRESH MATERIALIZED VIEW ErststimmenKandidatStimmkreisUI;
 REFRESH MATERIALIZED VIEW Beste_Stimmkreise_ParteiUI;
